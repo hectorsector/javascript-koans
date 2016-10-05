e661aed19867255b36aa06b37d4b23bc6d1e594a
Hector Alfaro <hectorsector@github.com>
2016-10-05 08:10:06 -0400

    Update AboutExpects.js

--- koans/AboutExpects.js
+++ koans/AboutExpects.js
@@ -4,7 +4,7 @@ describe('About Expects', function() {
   it('should expect true', function() {
 
     // Your journey begins here: Replace the word false with true 
-    expect(true).toBeTruthy();
+    expect(True).toBeTruthy();
   });
 
   // To understand reality, we must compare our expectations against reality.
